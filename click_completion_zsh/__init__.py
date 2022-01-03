74c855cdc855e662c4ac76a08b9dd6d3760ce329
Matthew Martin <phy1729@gmail.com>
2022-01-02 22:38:09 -0600

    Initial commit

---
+++ click_completion_zsh/__init__.py
@@ -0,0 +1,273 @@
+from os import environ
+from re import match
+from re import sub
+from typing import Any
+from typing import Optional
+
+from click import Command
+from click import Context
+from click import MultiCommand
+from click import ParamType
+from click.shell_completion import add_completion_class
+
+
+__all__ = ('init',)
+
+
+def quote(
+    value: str,
+    embed: bool = False,
+    double: bool = False,
+) -> str:
+    if match('^[A-Za-z0-9_-]*$', value):
+        return value
+
+    if double:
+        value = sub(r'([!$\\"`])', r'\\\1', value)
+    else:
+        value = value.replace("'", "'\\''")
+
+    if embed:
+        return value
+    else:
+        quote = '"' if double else "'"
+        return f'{quote}{value}{quote}'
+
+
+def get_help(
+    param: dict[str, Any],
+) -> Optional[str]:
+    help = param.get('short_help')
+    if help is None:
+        help = param.get('help')
+    if help is None:
+        return None
+
+    assert isinstance(help, str)
+
+    if param['name'] == 'help':
+        return 'display usage information'
+
+    help = help.rstrip('.')
+    first_word = help.split(' ')[0]
+    if all(not c.isupper() for c in first_word[1:]):
+        help = f'{help[0].lower()}{help[1:]}'
+
+    return help
+
+
+def complete(
+    info: dict[str, Any],
+) -> str:
+    lines = [
+        f'#compdef {info["command"]["name"]}',
+        '',
+    ]
+    if 'commands' in info['command']:
+        lines.extend((
+            'local curcontext="$curcontext" state state_descr line',
+            'typeset -A opt_args',
+            '',
+        ))
+
+    lines.extend(complete_command(info['command'], info['allow_interspersed_args']))
+    return ''.join(f'{line}\n' for line in lines)
+
+
+def complete_command(
+    command: dict[str, Any],
+    allow_interspersed_args: bool,
+) -> list[str]:
+    lines = []
+
+    line = '_arguments -s -S'
+    if not allow_interspersed_args:
+        line += ' -A \'-*\''
+    if 'commands' in command:
+        line += ' -C'
+    line += ' : \\'
+    lines.append(line)
+
+    specs = []
+    arg_count = 0
+    for param in command['params']:
+        if param['param_type_name'] == 'option':
+            spec = '\''
+
+            all_names = [*param['opts'], *param['secondary_opts']]
+
+            if param['multiple'] or param['count']:
+                spec += '*'
+            elif len(all_names) > 1:
+                spec += f'({" ".join(quote(opt, embed=True) for opt in all_names)})'
+
+            if len(all_names) == 1:
+                spec += quote(all_names[0], True)
+            else:
+                spec += f'\'{{{",".join(quote(opt, embed=True) for opt in all_names)}}}\''
+
+            help = get_help(param)
+            if help is not None:
+                spec += f'[{quote(help, embed=True)}]'
+            if not (param['is_flag'] or param['count']):
+                argspec = complete_type(param['type'], param['name'], param['default'])
+                # Tuple repatition is handled in complete_type
+                count = 1 if param['type']['param_type'] == 'Tuple' else param['nargs']
+                spec += ' '.join([argspec] * count)
+
+            spec += '\''
+
+            specs.append(spec)
+
+        elif param['param_type_name'] == 'argument':
+            arg_count += 1
+            spec = '\''
+            if param['nargs'] == -1:
+                spec += '*'
+            spec += complete_type(param['type'], param['name'], param['default'])
+            spec += '\''
+            count = 1 if param['nargs'] == -1 else param['nargs']
+            specs.extend([spec] * count)
+
+        else:
+            raise ValueError(f'Unexpected param_type_name {param["param_type_name"]}')  # pragma: no cover
+
+    if 'commands' in command:
+        subcommands = []
+        for name, subcommand in command['commands'].items():
+            help = get_help(subcommand)
+            if help is not None:
+                subcommands.append(f'"{quote(name, double=True, embed=True)}\\:{quote(help, double=True, embed=True)}"')
+            else:
+                subcommands.append(quote(name, double=True))
+        specs.append(f'\':subcommand:(({" ".join(subcommands)}))\'')
+        specs.append('\'*::: := ->subcmd\' && return 0')
+
+    lines.extend(f'  {spec} \\' for spec in specs[:-1])
+    lines.append(f'  {specs[-1]}')
+
+    if 'commands' in command:
+        lines.append('')
+        lines.append(f'service=$line[{arg_count + 1}]')
+        lines.append('curcontext=${curcontext%:*}-$service:')
+        lines.append('case $service in')
+
+        for name, subcommand in command['commands'].items():
+            lines.append(f'  {quote(name)})')
+            lines.extend(f'    {line}' for line in complete_command(subcommand, allow_interspersed_args))
+            lines.append('  ;;')
+
+        lines.append('esac')
+
+    return lines
+
+
+def complete_type(
+    param: dict[str, Any],
+    name: str,
+    default: Optional[Any],
+) -> str:
+    param_type = param['param_type']
+
+    # Strip trailing _ used to work around reserved words.
+    message = name[:-1] if name[-1] == '_' else name
+
+    if default is not None:
+        message += f' [{default}]'
+
+    if param_type == 'Bool':
+        return f':{message}:(0 1 false true f t no yes n y off on)'
+
+    if param_type == 'Choice':
+        return f':{message}:({" ".join(param["choices"])})'
+
+    if param_type == 'File':
+        return f':{message}:_files'
+
+    if param_type == 'Path':
+        only_dirs = param['file_okay'] is False and param['dir_okay'] is True
+        return f':{message}:_files{" -/" if only_dirs else ""}'
+
+    if param_type in ('IntRange', 'FloatRange'):
+        action = '_numbers'
+        if param_type == 'FloatRange':
+            action += ' -f'
+        if default is not None:
+            action += f' -d {default}'
+        if param['min'] is not None:
+            min_value = param['min']
+            if param_type == 'IntRange' and param['min_open']:
+                min_value += 1
+            action += f' -l {min_value}'
+        if param['max'] is not None:
+            max_value = param['max']
+            if param_type == 'IntRange' and param['max_open']:
+                max_value -= 1
+            action += f' -m {max_value}'
+        action += ' ' + quote(name, double=True)
+        return f': :{action}'
+
+    if param_type == 'Tuple':
+        return ' '.join(complete_type(argument, name, None)
+                        for argument in param['types'])
+
+    return f':{message}:'
+
+
+def find_param_type(
+    name: str,
+    ctx: Context,
+    command: Command,
+) -> Optional[ParamType]:
+    for param in command.get_params(ctx):
+        if param.type.name == name:
+            return param.type
+
+    if isinstance(command, MultiCommand):
+        for subcommand_name in command.list_commands(ctx):
+            subcommand = command.get_command(ctx, subcommand_name)
+            if subcommand is None:
+                continue  # pragma: no cover
+            result = find_param_type(name, ctx, subcommand)
+            if result is not None:
+                return result
+
+    return None
+
+
+class Zsh2Complete:
+    name = 'zsh2'
+
+    def __init__(
+        self,
+        cli: Command,
+        ctx_args: dict[str, Any],
+        prog_name: str,
+        complete_var: str,
+    ) -> None:
+        self.cli = cli
+        self.ctx_args = ctx_args
+        self.prog_name = prog_name
+        self.complete_var = complete_var
+
+    def source(self) -> str:
+        """Produce the completion script."""
+        with Context(self.cli) as ctx:
+            info = ctx.to_info_dict()
+        return complete(info)
+
+    def complete(self) -> str:
+        """Produce completions for a type."""
+        with Context(self.cli) as ctx:
+            param_type = find_param_type(environ['COMP_TYPE'], ctx, ctx.command)
+            if param_type is None:
+                return ''
+            completions = param_type.shell_complete(ctx, None, '')
+        return '\0'.join(f'{x.value}\0{x.help if x.help is not None else ""}'
+                         for x in completions)
+
+
+def init() -> None:
+    # Zsh2Complete doesn't inherit from ShellComplete, but the duck type
+    # matches.
+    add_completion_class(Zsh2Complete, 'zsh2')  # type: ignore
---
+++ click_completion_zsh/test/test_init.py
@@ -0,0 +1,321 @@
+from os import environ
+from textwrap import dedent
+
+import click
+from click import Context
+from click import ParamType
+from click import Parameter
+from click.shell_completion import CompletionItem
+from click.shell_completion import get_completion_class
+
+from .. import Zsh2Complete
+from .. import find_param_type
+from .. import get_help
+from .. import init
+from .. import quote
+
+
+def test_quote() -> None:
+    assert quote('foo_bar') == 'foo_bar'
+    assert quote('foo bar') == "'foo bar'"
+    assert quote("foo's bar") == "'foo'\\''s bar'"
+    assert quote('foo_bar', double=True) == 'foo_bar'
+    assert quote('foo bar', double=True) == '"foo bar"'
+    assert quote('$(foo "bar" `baz`', double=True) == r'"\$(foo \"bar\" \`baz\`"'
+    assert quote("foo's bar", embed=True) == "foo'\\''s bar"
+
+
+def test_get_help() -> None:
+    assert get_help({}) is None
+    assert get_help({
+        'name': 'help',
+        'param_type_name': 'option',
+        'opts': ['--help'],
+        'secondary_opts': [],
+        'type': {
+            'param_type': 'Bool',
+            'name': 'boolean',
+        },
+        'required': False,
+        'nargs': 1,
+        'multiple': False,
+        'default': False,
+        'envvar': None,
+        'help': 'Show this message and exit.',
+        'prompt': None,
+        'is_flag': True,
+        'flag_value': True,
+        'count': False,
+        'hidden': False,
+    }) == 'display usage information'
+    assert get_help({
+        'name': 'verbose',
+        'opts': ['--verbose'],
+        'help': 'Be more verbose.'
+    }) == 'be more verbose'
+    assert get_help({
+        'name': 'foo',
+        'help': 'Some long description goes here.',
+        'short_help': 'Concise text.',
+    }) == 'concise text'
+    assert get_help({
+        'name': 'hostname',
+        'help': 'LDAP hostname.',
+    }) == 'LDAP hostname'
+
+
+def test_options() -> None:
+    # Cases from https://click.palletsprojects.com/en/8.0.x/options/
+    for decorator, line in (
+        (click.option('--n', default=1), '--n:n [1]:'),
+        (click.option('--n', required=True, type=int), '--n:n:'),
+        (click.option('--from', '-f', 'from_'), '(--from -f)\'{--from,-f}\':from:'),
+        (click.option('--to', '-t'), '(--to -t)\'{--to,-t}\':to:'),
+        (click.option('--pos', nargs=2, type=float), '--pos:pos: :pos:'),
+        (click.option('--item', type=(str, int)), '--item:item: :item:'),
+        (click.option('--message', '-m', multiple=True), '*\'{--message,-m}\':message:'),
+        (click.option('-v', '--verbose', count=True), '*\'{-v,--verbose}\''),
+        (click.option('--shout/--no-shout', default=False), '(--shout --no-shout)\'{--shout,--no-shout}\''),
+        (click.option('--shout', is_flag=True), '--shout'),
+        (click.option('--shout/--no-shout', ' /-S', default=False), '(--shout --no-shout -S)\'{--shout,--no-shout,-S}\''),
+        (click.option('--hash-type', type=click.Choice(['MD5', 'SHA1'], case_sensitive=False)), '--hash-type:hash_type:(MD5 SHA1)'),
+        (click.option('--name', prompt=True), '--name:name:'),
+        (click.password_option(), '--password:password:'),
+        (click.confirmation_option(prompt='Are you sure you want to drop the db?'), '--yes[confirm the action without prompting]'),
+        (click.option('+w/-w'), '(+w -w)\'{+w,-w}\''),
+        (click.option("--count", type=click.IntRange(0, 20, clamp=True)), '--count: :_numbers -l 0 -m 20 count'),
+        (click.option("--digit", type=click.IntRange(0, 9)), '--digit: :_numbers -l 0 -m 9 digit'),
+        # XXX mark optional
+        (click.option("--name", is_flag=False, flag_value="Flag", default="Default"), '--name:name [Default]:'),
+    ):
+        @click.command()
+        @decorator
+        def cli() -> None:
+            """Dummy command for testing."""
+
+        assert Zsh2Complete(cli, {}, 'cli', '').source() == dedent(r'''
+                #compdef cli
+
+                _arguments -s -S : \
+                  ''' + f"'{line}'" + r''' \
+                  '--help[display usage information]'
+                ''')[1:]
+
+
+def test_arguments() -> None:
+    # Cases from https://click.palletsprojects.com/en/8.0.x/arguments/
+    for decorator, line in (
+        (click.argument('filename'), ':filename:'),
+        (click.argument('src', nargs=-1), '*:src:'),
+        (click.argument('input', type=click.File('rb')), ':input:_files'),
+        (click.argument('filename', type=click.Path(exists=True)), ':filename:_files'),
+    ):
+        @click.command()
+        @decorator
+        def cli() -> None:
+            """Dummy command for testing."""
+
+        assert Zsh2Complete(cli, {}, 'cli', '').source() == dedent(r'''
+                #compdef cli
+
+                _arguments -s -S : \
+                  ''' + f"'{line}'" + r''' \
+                  '--help[display usage information]'
+                ''')[1:]
+
+
+def test_types() -> None:
+    for decorator, line in (
+        (click.argument('light', type=click.BOOL), ':light:(0 1 false true f t no yes n y off on)'),
+        (click.argument('rotation', type=click.FloatRange(0, 360), default=0), ': :_numbers -f -d 0 -l 0 -m 360 rotation'),
+        (click.argument('hour', type=click.IntRange(0, 24, max_open=True)), ': :_numbers -l 0 -m 23 hour'),
+        (click.argument('rate', type=click.IntRange(0, 100, min_open=True)), ': :_numbers -l 1 -m 100 rate'),
+        (click.argument('file', type=click.File('r'), default='foo'), ':file [foo]:_files'),
+        (click.argument('dir', type=click.Path(file_okay=False)), ':dir:_files -/'),
+    ):
+        @click.command()
+        @decorator
+        def cli() -> None:
+            """Dummy command for testing."""
+
+        assert Zsh2Complete(cli, {}, 'cli', '').source() == dedent(r'''
+                #compdef cli
+
+                _arguments -s -S : \
+                  ''' + f"'{line}'" + r''' \
+                  '--help[display usage information]'
+                ''')[1:]
+
+
+def test_command_variadic() -> None:
+    @click.command()
+    @click.argument('src', nargs=-1, type=click.File('r'))
+    @click.argument('dest', nargs=1, type=click.File('w'))
+    def cli() -> None:
+        """Dummy command for testing."""
+
+    assert Zsh2Complete(cli, {}, 'cli', '').source() == dedent(r'''
+            #compdef cli
+
+            _arguments -s -S : \
+              '*:src:_files' \
+              ':dest:_files' \
+              '--help[display usage information]'
+            ''')[1:]
+
+
+def test_command_subcommands() -> None:
+    @click.group()
+    @click.option('-v', '--verbose', count=True, help='Increase verbosity.')
+    def cli() -> None:
+        """Dummy command for testing."""
+
+    @cli.command()
+    @click.option('-b', '--bar', help='bar\'s help')
+    def foo() -> None:
+        """Foo subcommand help."""
+
+    @cli.command()
+    @click.option('--foo', default=1, help='foo help')
+    def bar() -> None:
+        pass  # pragma: no cover
+
+    assert Zsh2Complete(cli, {}, 'cli', '').source() == dedent(r'''
+            #compdef cli
+
+            local curcontext="$curcontext" state state_descr line
+            typeset -A opt_args
+
+            _arguments -s -S -A '-*' -C : \
+              '*'{-v,--verbose}'[increase verbosity]' \
+              '--help[display usage information]' \
+              ':subcommand:((bar "foo\:foo subcommand help"))' \
+              '*::: := ->subcmd' && return 0
+
+            service=$line[1]
+            curcontext=${curcontext%:*}-$service:
+            case $service in
+              bar)
+                _arguments -s -S -A '-*' : \
+                  '--foo[foo help]:foo [1]:' \
+                  '--help[display usage information]'
+              ;;
+              foo)
+                _arguments -s -S -A '-*' : \
+                  '(-b --bar)'{-b,--bar}'[bar'\''s help]:bar:' \
+                  '--help[display usage information]'
+              ;;
+            esac
+            ''')[1:]
+
+
+def test_command_subcommand_after_arg() -> None:
+    @click.group()
+    @click.argument('arg')
+    def cli() -> None:
+        """Dummy command for testing."""
+
+    @cli.command()
+    @click.argument('foo')
+    def foo() -> None:
+        """Foo."""
+
+    @cli.command()
+    @click.argument('bar')
+    def bar() -> None:
+        """Bar."""
+
+    assert Zsh2Complete(cli, {}, 'cli', '').source() == dedent(r'''
+            #compdef cli
+
+            local curcontext="$curcontext" state state_descr line
+            typeset -A opt_args
+
+            _arguments -s -S -A '-*' -C : \
+              ':arg:' \
+              '--help[display usage information]' \
+              ':subcommand:(("bar\:bar" "foo\:foo"))' \
+              '*::: := ->subcmd' && return 0
+
+            service=$line[2]
+            curcontext=${curcontext%:*}-$service:
+            case $service in
+              bar)
+                _arguments -s -S -A '-*' : \
+                  ':bar:' \
+                  '--help[display usage information]'
+              ;;
+              foo)
+                _arguments -s -S -A '-*' : \
+                  ':foo:' \
+                  '--help[display usage information]'
+              ;;
+            esac
+            ''')[1:]
+
+
+def test_find_param_type() -> None:
+    class TestType(ParamType):
+        name = 'test'
+
+    @click.group()
+    @click.argument('arg')
+    def cli() -> None:
+        """Dummy command for testing."""
+
+    @cli.command()
+    @click.argument('foo')
+    def foo() -> None:
+        """Foo."""
+
+    @cli.group()
+    @click.argument('bar')
+    def bar() -> None:
+        """Bar."""
+
+    @bar.command()
+    @click.argument('baz', type=TestType())
+    def baz() -> None:
+        """Baz."""
+
+    with Context(cli) as ctx:
+        assert isinstance(find_param_type('test', ctx, cli), TestType)
+        assert find_param_type('text', ctx, cli) is click.STRING
+        # While bool is a valid type defined by click, since it is not used by the
+        # test command, it should not be found.
+        assert find_param_type('bool', ctx, cli) is None
+
+
+def test_complete() -> None:
+    class TestType(ParamType):
+        name = 'test'
+
+        def shell_complete(
+                self,
+                ctx: Context,
+                param: Parameter,
+                incomplete: str
+            ) -> list[CompletionItem]:
+            return [CompletionItem(x, help=f'{x} help')
+                    for x in ('foo', 'bar', 'baz')
+                    if x.startswith(incomplete)]
+
+    @click.command()
+    @click.argument('arg', type=TestType())
+    def cli() -> None:
+        """Dummy command for testing."""
+
+    environ['COMP_TYPE'] = 'test'
+    assert Zsh2Complete(cli, {}, 'cli', '').complete() == (
+        'foo\0foo help\0'
+        'bar\0bar help\0'
+        'baz\0baz help'
+    )
+
+    environ['COMP_TYPE'] = 'does_not_exist'
+    assert Zsh2Complete(cli, {}, 'cli', '').complete() == ''
+
+
+def test_init() -> None:
+    init()
+    assert get_completion_class('zsh2') is Zsh2Complete  # type: ignore
