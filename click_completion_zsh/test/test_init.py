e276b07a240021954fe90557fcc1468e27081654
Matthew Martin <phy1729@gmail.com>
2022-01-05 22:32:56 -0600

    Refactor tests to use pytest.mark.parametrize

--- click_completion_zsh/test/test_init.py
+++ click_completion_zsh/test/test_init.py
@@ -1,5 +1,6 @@
 from os import environ
 from textwrap import dedent
+from typing import Any
 
 import click
 from click import Context
@@ -7,6 +8,7 @@ from click import ParamType
 from click import Parameter
 from click.shell_completion import CompletionItem
 from click.shell_completion import get_completion_class
+from pytest import mark
 
 from .. import Zsh2Complete
 from .. import find_param_type
@@ -29,19 +31,26 @@ class FooType(ParamType):
                 if x.startswith(incomplete)]
 
 
-def test_quote() -> None:
-    assert quote('foo_bar') == 'foo_bar'
-    assert quote('foo bar') == "'foo bar'"
-    assert quote("foo's bar") == "'foo'\\''s bar'"
-    assert quote('foo_bar', double=True) == 'foo_bar'
-    assert quote('foo bar', double=True) == '"foo bar"'
-    assert quote('$(foo "bar" `baz`', double=True) == r'"\$(foo \"bar\" \`baz\`"'
-    assert quote("foo's bar", embed=True) == "foo'\\''s bar"
+@mark.parametrize(('input', 'kwargs', 'output'), (
+    ('foo_bar', {}, 'foo_bar'),
+    ('foo bar', {}, "'foo bar'"),
+    ("foo's bar", {}, "'foo'\\''s bar'"),
+    ('foo_bar', {'double': True}, 'foo_bar'),
+    ('foo bar', {'double': True}, '"foo bar"'),
+    ('$(foo "bar" `baz`', {'double': True}, r'"\$(foo \"bar\" \`baz\`"'),
+    ("foo's bar", {'embed': True}, "foo'\\''s bar"),
+))
+def test_quote(
+    input: str,
+    kwargs: dict[str, Any],
+    output: str,
+) -> None:
+    assert quote(input, **kwargs) == output
 
 
-def test_get_help() -> None:
-    assert get_help({}) is None
-    assert get_help({
+@mark.parametrize(('input', 'output'), (
+    ({}, None),
+    ({
         'name': 'help',
         'param_type_name': 'option',
         'opts': ['--help'],
@@ -61,106 +70,90 @@ def test_get_help() -> None:
         'flag_value': True,
         'count': False,
         'hidden': False,
-    }) == 'display usage information'
-    assert get_help({
+    }, 'display usage information'),
+    ({
         'name': 'verbose',
         'opts': ['--verbose'],
         'help': 'Be more verbose.'
-    }) == 'be more verbose'
-    assert get_help({
+    }, 'be more verbose'),
+    ({
         'name': 'foo',
         'help': 'Some long description goes here.',
         'short_help': 'Concise text.',
-    }) == 'concise text'
-    assert get_help({
+    }, 'concise text'),
+    ({
         'name': 'hostname',
         'help': 'LDAP hostname.',
-    }) == 'LDAP hostname'
+    }, 'LDAP hostname'),
+))
+def test_get_help(
+    input: dict[str, Any],
+    output: str
+) -> None:
+    assert get_help(input) == output
 
 
-def test_options() -> None:
+@mark.parametrize(('decorator', 'line'), (
     # Cases from https://click.palletsprojects.com/en/8.0.x/options/
-    for decorator, line in (
-        (click.option('--n', default=1), '--n:n [1]:'),
-        (click.option('--n', required=True, type=int), '--n:n:'),
-        (click.option('--from', '-f', 'from_'), '(--from -f)\'{--from,-f}\':from:'),
-        (click.option('--to', '-t'), '(--to -t)\'{--to,-t}\':to:'),
-        (click.option('--pos', nargs=2, type=float), '--pos:pos: :pos:'),
-        (click.option('--item', type=(str, int)), '--item:item: :item:'),
-        (click.option('--message', '-m', multiple=True), '*\'{--message,-m}\':message:'),
-        (click.option('-v', '--verbose', count=True), '*\'{-v,--verbose}\''),
-        (click.option('--shout/--no-shout', default=False), '(--shout --no-shout)\'{--shout,--no-shout}\''),
-        (click.option('--shout', is_flag=True), '--shout'),
-        (click.option('--shout/--no-shout', ' /-S', default=False), '(--shout --no-shout -S)\'{--shout,--no-shout,-S}\''),
-        (click.option('--hash-type', type=click.Choice(['MD5', 'SHA1'], case_sensitive=False)), '--hash-type:hash_type:(MD5 SHA1)'),
-        (click.option('--name', prompt=True), '--name:name:'),
-        (click.password_option(), '--password:password:'),
-        (click.confirmation_option(prompt='Are you sure you want to drop the db?'), '--yes[confirm the action without prompting]'),
-        (click.option('+w/-w'), '(+w -w)\'{+w,-w}\''),
-        (click.option("--count", type=click.IntRange(0, 20, clamp=True)), '--count: :_numbers -l 0 -m 20 count'),
-        (click.option("--digit", type=click.IntRange(0, 9)), '--digit: :_numbers -l 0 -m 9 digit'),
-        # XXX mark optional
-        (click.option("--name", is_flag=False, flag_value="Flag", default="Default"), '--name:name [Default]:'),
-    ):
-        @click.command()
-        @decorator
-        def cli() -> None:
-            """Dummy command for testing."""
+    (click.option('--n', default=1), '--n:n [1]:'),
+    (click.option('--n', required=True, type=int), '--n:n:'),
+    (click.option('--from', '-f', 'from_'), '(--from -f)\'{--from,-f}\':from:'),
+    (click.option('--to', '-t'), '(--to -t)\'{--to,-t}\':to:'),
+    (click.option('--pos', nargs=2, type=float), '--pos:pos: :pos:'),
+    (click.option('--item', type=(str, int)), '--item:item: :item:'),
+    (click.option('--message', '-m', multiple=True), '*\'{--message,-m}\':message:'),
+    (click.option('-v', '--verbose', count=True), '*\'{-v,--verbose}\''),
+    (click.option('--shout/--no-shout', default=False), '(--shout --no-shout)\'{--shout,--no-shout}\''),
+    (click.option('--shout', is_flag=True), '--shout'),
+    (click.option('--shout/--no-shout', ' /-S', default=False), '(--shout --no-shout -S)\'{--shout,--no-shout,-S}\''),
+    (click.option('--hash-type', type=click.Choice(['MD5', 'SHA1'], case_sensitive=False)), '--hash-type:hash_type:(MD5 SHA1)'),
+    (click.option('--name', prompt=True), '--name:name:'),
+    (click.password_option(), '--password:password:'),
+    (click.confirmation_option(prompt='Are you sure you want to drop the db?'), '--yes[confirm the action without prompting]'),
+    (click.option('+w/-w'), '(+w -w)\'{+w,-w}\''),
+    (click.option("--count", type=click.IntRange(0, 20, clamp=True)), '--count: :_numbers -l 0 -m 20 count'),
+    (click.option("--digit", type=click.IntRange(0, 9)), '--digit: :_numbers -l 0 -m 9 digit'),
+    # XXX mark optional
+    (click.option("--name", is_flag=False, flag_value="Flag", default="Default"), '--name:name [Default]:'),
 
-        assert Zsh2Complete(cli, {}, 'cli', '').source() == dedent(r'''
-                #compdef cli
-
-                _arguments -s -S : \
-                  ''' + f"'{line}'" + r''' \
-                  '--help[display usage information]'
-                ''')[1:]
-
-
-def test_arguments() -> None:
     # Cases from https://click.palletsprojects.com/en/8.0.x/arguments/
-    for decorator, line in (
-        (click.argument('filename'), ':filename:'),
-        (click.argument('src', nargs=-1), '*:src:'),
-        (click.argument('input', type=click.File('rb')), ':input:_files'),
-        (click.argument('filename', type=click.Path(exists=True)), ':filename:_files'),
-    ):
-        @click.command()
-        @decorator
-        def cli() -> None:
-            """Dummy command for testing."""
+    (click.argument('filename'), ':filename:'),
+    (click.argument('src', nargs=-1), '*:src:'),
+    (click.argument('input', type=click.File('rb')), ':input:_files'),
+    (click.argument('filename', type=click.Path(exists=True)), ':filename:_files'),
 
-        assert Zsh2Complete(cli, {}, 'cli', '').source() == dedent(r'''
-                #compdef cli
+    # Additional tests for types
+    (click.argument('light', type=click.BOOL), ':light:(0 1 false true f t no yes n y off on)'),
+    (click.argument('rotation', type=click.FloatRange(0, 360), default=0), ': :_numbers -f -d 0 -l 0 -m 360 rotation'),
+    (click.argument('hour', type=click.IntRange(0, 24, max_open=True)), ': :_numbers -l 0 -m 23 hour'),
+    (click.argument('rate', type=click.IntRange(0, 100, min_open=True)), ': :_numbers -l 1 -m 100 rate'),
+    (click.argument('score', type=click.IntRange(0)), ': :_numbers -l 0 score'),
+    (click.argument('grav_potential', type=click.IntRange(max=0)), ': :_numbers -m 0 grav_potential'),
+    (click.argument('file', type=click.File('r'), default='foo'), ':file [foo]:_files'),
+    (click.argument('dir', type=click.Path(file_okay=False)), ':dir:_files -/'),
 
-                _arguments -s -S : \
-                  ''' + f"'{line}'" + r''' \
-                  '--help[display usage information]'
-                ''')[1:]
+    # option that needs quoting
+    (click.option('--terrible', '-$'), '(--terrible -$)\'{--terrible,\'-$\'}\':terrible:'),
 
+    # bracket in help
+    (click.option('--test', help='test suite [default: default]'), r'--test[test suite \[default: default\]]:test:'),
+))
+def test_source(
+    decorator,
+    line: str,
+) -> None:
+    @click.command()
+    @decorator
+    def cli() -> None:
+        """Dummy command for testing."""
 
-def test_types() -> None:
-    for decorator, line in (
-        (click.argument('light', type=click.BOOL), ':light:(0 1 false true f t no yes n y off on)'),
-        (click.argument('rotation', type=click.FloatRange(0, 360), default=0), ': :_numbers -f -d 0 -l 0 -m 360 rotation'),
-        (click.argument('hour', type=click.IntRange(0, 24, max_open=True)), ': :_numbers -l 0 -m 23 hour'),
-        (click.argument('rate', type=click.IntRange(0, 100, min_open=True)), ': :_numbers -l 1 -m 100 rate'),
-        (click.argument('score', type=click.IntRange(0)), ': :_numbers -l 0 score'),
-        (click.argument('grav_potential', type=click.IntRange(max=0)), ': :_numbers -m 0 grav_potential'),
-        (click.argument('file', type=click.File('r'), default='foo'), ':file [foo]:_files'),
-        (click.argument('dir', type=click.Path(file_okay=False)), ':dir:_files -/'),
-    ):
-        @click.command()
-        @decorator
-        def cli() -> None:
-            """Dummy command for testing."""
+    assert Zsh2Complete(cli, {}, 'cli', '').source() == dedent(r'''
+            #compdef cli
 
-        assert Zsh2Complete(cli, {}, 'cli', '').source() == dedent(r'''
-                #compdef cli
-
-                _arguments -s -S : \
-                  ''' + f"'{line}'" + r''' \
-                  '--help[display usage information]'
-                ''')[1:]
+            _arguments -s -S : \
+              ''' + f"'{line}'" + r''' \
+              '--help[display usage information]'
+            ''')[1:]
 
 
 def test_command_name() -> None:
@@ -176,21 +169,6 @@ def test_command_name() -> None:
             ''')[1:]
 
 
-def test_option_needs_quoting() -> None:
-    @click.command()
-    @click.option('--terrible', '-$')
-    def cli() -> None:
-        """Dummy command for testing."""
-
-    assert Zsh2Complete(cli, {}, 'cli', '').source() == dedent(r'''
-            #compdef cli
-
-            _arguments -s -S : \
-              '(--terrible -$)'{--terrible,'-$'}':terrible:' \
-              '--help[display usage information]'
-            ''')[1:]
-
-
 def test_option_non_dash_prefix() -> None:
     @click.command(context_settings={'allow_interspersed_args': False})
     @click.option('+w/-w')
@@ -202,21 +180,6 @@ def test_option_non_dash_prefix() -> None:
 
             _arguments -s -S -A '[+-]*' : \
               '(+w -w)'{+w,-w}'' \
-              '--help[display usage information]'
-            ''')[1:]
-
-
-def test_option_bracket_in_help() -> None:
-    @click.command()
-    @click.option('--test', help='test suite [default: default]')
-    def cli() -> None:
-        """Dummy command for testing."""
-
-    assert Zsh2Complete(cli, {}, 'cli', '').source() == dedent(r'''
-            #compdef cli
-
-            _arguments -s -S : \
-              '--test[test suite \[default: default\]]:test:' \
               '--help[display usage information]'
             ''')[1:]
 
